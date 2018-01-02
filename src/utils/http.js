6610736f8cd3010fa0b6d94ff9e14177b2d01cb3
yangxiao <842396362@qq.com>
2018-01-02 15:03:47 +0800

    一体机后台

--- src/utils/http.js
+++ src/utils/http.js
@@ -8,7 +8,7 @@ import { Message } from 'element-ui';
 import store from "../store/userinfo/user"
 
 var  service=axios.create({
-  baseURL: 'http://192.168.1.188:8081/aiom', // api的base_url yyx-house-expor-api
+  baseURL: 'http://192.168.1.188:9000/aiom', // api的base_url yyx-house-expor-api
   timeout: 5000,                  // 请求超时时间
   transformRequest: [function (data) {
     // 这里可以在发送请求之前对请求数据做处理，比如form-data格式化等，这里可以使用开头引入的Qs（这个模块在安装axios的时候就已经安装了，不需要另外安装）
